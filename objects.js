975cdf28848065fb88f124f2dea45c8ea1bcbaf1
Joanne Basa <joannegbasa@gmail.com>
2017-10-13 13:58:10 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {'Cut to the feeling': 'Carly Rae Jepsen'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
